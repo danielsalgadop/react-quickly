76f581c28a54ed03d750f19d78db2aa541c61727
danielsalgadop <daniel.salgado.p@gmail.com>
2019-10-05 18:44:45 +0200

    playing with ch01

---
+++ ch01/hello-world/js/in_script.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+  React.createElement('h1', null, 'In script Text, to avoid "Target container is not a DOM element" DOM element be defined before this script'),
+  document.getElementById('in_script')
+)
